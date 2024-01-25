9cd5d1c78ff359cc0260bd76dadfeffb9f136afd
Sebastian Quilitz <zeraphim@x-pantion.de>
2024-01-25 20:04:45 +0000

    Add missing Copyright header to the new files

--- GbDma.h
+++ GbDma.h
@@ -1,3 +1,20 @@
+/* RP2040 GameBoy cartridge
+ * Copyright (C) 2024 Sebastian Quilitz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef D2C8524D_9F5F_4D9E_BD87_3A37DED846AC
 #define D2C8524D_9F5F_4D9E_BD87_3A37DED846AC
 
--- ws2812b_spi.h
+++ ws2812b_spi.h
@@ -1,3 +1,20 @@
+/* RP2040 GameBoy cartridge
+ * Copyright (C) 2024 Sebastian Quilitz
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #ifndef A9FD9FC9_43F8_4ADF_936A_6ADF1E1699A5
 #define A9FD9FC9_43F8_4ADF_936A_6ADF1E1699A5
 
