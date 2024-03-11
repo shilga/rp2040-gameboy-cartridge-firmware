0db87d29dffdbbafccec229d2fa1c40aa8de72e7
Sebastian Quilitz <zeraphim@x-pantion.de>
2024-03-11 21:14:46 +0000

    switch UART baud to 576k

--- main.c
+++ main.c
@@ -100,7 +100,7 @@ int main() {
     sleep_ms(2);
   }
 
-  stdio_uart_init_full(uart0, 500000, 28, -1);
+  stdio_uart_init_full(uart0, 576000, 28, -1);
 
   printf("Hello RP2040 Croco Cartridge %d.%d.%d %s-%.7X(%s)\n",
          RP2040_GB_CARTRIDGE_VERSION_MAJOR, RP2040_GB_CARTRIDGE_VERSION_MINOR,
